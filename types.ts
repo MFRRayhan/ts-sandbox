c515b9cc2f3731cdd66deef2093cc02b1ee87640
MFRRayhan <md.fazlerabbirayhan786@gmail.com>
2026-07-31 06:25:35 +0600

    add multiply function with error handling and type examples

---
+++ types.ts
@@ -0,0 +1,9 @@
+const country: string = "Bangladesh";
+const digit: number = 10;
+const articleStatus: boolean = true;
+let x;
+
+console.log(typeof country);
+console.log(typeof digit);
+console.log(typeof articleStatus);
+console.log(typeof x);
